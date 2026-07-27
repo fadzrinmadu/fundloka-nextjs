cfddd7dd0d2e63b40788c0019ed840b9623d56fa
Muhammad Fadzrin Madu <muhfadzrinmadu@gmail.com>
2026-07-27 16:38:57 +0700

    chore: adjust image on login and register page

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -29,9 +29,9 @@ export default function RegisterPage() {
   }
 
   return (
-    <div className="min-h-screen flex justify-center items-center py-10">
-      <div className="hidden md:block lg:w-1/3 bg-white h-full rounded-tr-lg rounded-br-lg auth-background-register" />
-      <div className="w-auto md:w-2/4 lg:w-2/3 flex justify-center items-center">
+    <div className="min-h-screen flex justify-center">
+      <div className="hidden md:block lg:w-1/3 bg-white rounded-tr-lg rounded-br-lg auth-background-register" />
+      <div className="w-auto md:w-2/4 lg:w-2/3 flex justify-center items-center py-10">
         <div className="w-full lg:w-1/2 px-10 lg:px-0">
           <h2 className="font-normal mb-6 text-3xl text-white">Sign Up Account</h2>
 
